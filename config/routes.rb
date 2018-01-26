be54ebf0a0f4cf7029d06f47e7b95fc7eaaad533
rails blog <kontactrohit@gmail.com>
2018-01-27 00:52:50 +0530

    article few part

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html hk lkjkl jk j jj j j j j jjjjjjjjjjjjjjjj
 
 get 'pages/home', to: 'pages#home'
-
+resources :articles
 end
