5315bc0282f8eb733db637967c6b7e022aeda588
rails blog <kontactrohit@gmail.com>
2018-01-26 20:17:02 +0530

    home page change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html hk lkjkl jk j jj j j j j jjjjjjjjjjjjjjjj
+
+get 'pages/home', to: 'pages#home'
+
 end
