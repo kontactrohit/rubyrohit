d3aa9e65547a43890c17ca6a4df7537993a1be98
rails blog <kontactrohit@gmail.com>
2018-02-15 21:35:40 +0530

    bootstrap added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,59 +1,61 @@
 class ArticlesController < ApplicationController
 
-    def new
-        @article = Article.new
-    end
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-      def create
-        @article = Article.new(article_params)
-        if @article.save
-        flash[:notice] = "Article was successfully created"
-        redirect_to article_path(@article)
-        else
-        render 'new'
-        end
-      end
+def index
+@articles = Article.all
+end
 
-      def show
-        @article = Article.find(params[:id])
-      end
+def new
+@article = Article.new
+end
 
+def edit
+end
 
-      def edit
+def create
+@article = Article.new(article_params)
+if @article.save
+flash[:notice] = "Article was successfully created"
+redirect_to article_path(@article)
 
-      @article = Article.find(params[:id])
+else
 
-      end
+render 'new'
+end
 
-      def index
+end
 
-      @articles = Article.all
+def update
+if @article.update(article_params)
+flash[:notice] = "Article was successfully updated"
+redirect_to article_path(@article)
 
-      end
-      
+else
 
-      def update
+render 'edit'
+end
 
-      @article = Article.find(params[:id])
+end
 
-      if @article.update(article_params)
+def show
 
-      flash[:notice] = "Article was successfully updated"
+end
 
-      redirect_to article_path(@article)
+def destroy
+@article.destroy
+flash[:notice] = "Article was successfully deleted"
+redirect_to articles_path
+end
 
-      else
+private
 
-      render 'edit'
+def set_article
+@article = Article.find(params[:id])
+end
 
-      end
-
-      end
-
-
-    private
-      def article_params
-      params.require(:article).permit(:title, :description)
-    end
+def article_params
+params.require(:article).permit(:title, :description)
+end
 
 end
